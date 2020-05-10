56d51a91862318d1be2cd9b146d3b5618c326ee5
송동욱 <quino0627@korea.ac.kr>
2020-05-10 20:09:48 +0900

    unlogin router

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Auth from '../Routes/Auth';
 import Quest from '../Routes/Quest';
 import Feed from '../Routes/Feed';
@@ -15,6 +15,7 @@ const LoggedInRoutes = () => (
 const LoggedOutRoutes = () => (
   <Switch>
     <Route exact path="/" component={Auth} />
+    <Redirect path="*" to="/" />
   </Switch>
 );
 
